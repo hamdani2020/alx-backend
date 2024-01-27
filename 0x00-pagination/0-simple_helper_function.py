3e5f947cfdadc49d5ee255254f3f0a537b95f680
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-01-27 20:35:43 +0000

    I'm still finding the bug

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,11 +3,12 @@
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int: int]:
     """The funciton returns a tuple of size two containing a start
     index and and end index corresponding to the indexes to return
     in a list for those particular pagination parameters
     """
-    start = (page -1) * page_size
+    start = (page - 1) * page_size
     end = start + page_size
     return(start, end)
