f4f1c009070a0d4ca2922c1ad80166cedcb4da49
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-01-28 20:10:11 +0000

    I've fixed yesterday's bug, I used : instead of , in Tuple[int: int]

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,7 +4,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int: int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """The funciton returns a tuple of size two containing a start
     """
     return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
