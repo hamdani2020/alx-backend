5dc99722203959d440e5134a8829431887508ad4
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-01-30 01:08:31 +0000

    I'm still fixing some bugs

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""Module for hypermedia_del_pagination"""
+
+import csv
+import math
+from typing import List
+from typing import Tuple
+from typing import Dict
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """It returns a list of pagination parameters
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """It retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """It retrieves data about the from a particular index and with
+        with a specific size
+        """
+        data = self.indexed_dataset()
+        assert index is not None and index >= 0 and index <= max(data.keys())
+        page_data = []
+        data_count = 0
+        next_index = None
+        start = index if index else 0
+        for i, item in data.items():
+            if i >= start and data_count < page_size:
+                page_data.append(item)
+                data_count += 1
+                continue
+            if data_count == page_size:
+                next_index = -1
+                break
+            page_information = {
+                'index': index,
+                'next_index': next_index,
+                'page_size': len(page_data),
+                'data': page_data
+            }
+            return page_information
