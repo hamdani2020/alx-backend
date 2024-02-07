504af17969c86a773b9c73e67244cc4c146eb570
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-02-07 09:17:02 +0000

    I created a flask app and html templates

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This is a flask application
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def index():
+    """
+    This function renders the html file index.html
+    """
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
