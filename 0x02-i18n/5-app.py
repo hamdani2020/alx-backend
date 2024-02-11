53a40804e7e2709dbd8732d85276d718ad8c8ddc
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-02-11 01:33:38 +0000

    Mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+"""
+Flask application for translation
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Dict, Union
+
+
+class Config:
+    """
+    Flask babel configuration
+    """
+    BABEL_DEFAULT_LOCALE = "en"
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+users = {
+    1: {"name": "Amadu", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "LusiTech", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spy", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Hunter", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """
+    It retrieves the user based on user id
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """
+    It performs routines
+    """
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    It retrieves the locale for the web page
+    """
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+"""
+def get_user() -> Union[Dict, None]:
+
+    It retrieves the user based on user id
+    
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    
+    It performs routines
+    
+    user = get_user()
+    g.user = user
+"""
+
+
+@app.route('/')
+def get_index() -> str:
+    """
+    Index page
+    """
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
