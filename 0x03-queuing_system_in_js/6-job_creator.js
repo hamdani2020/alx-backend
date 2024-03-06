e17eae53a5a6c085851d0040fcad0da15ecc0e49
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-03-06 14:45:54 +0000

    Created the job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -5,8 +5,8 @@ import { createQueue } from 'kue';
 const queue = createQueue({name: 'push_notification_code'});
 
 const job = queue.create('push_notification_code', {
-  phoneNumber: '050930977',
-  message: 'Account registered',
+  phoneNumber: '000930977',
+  message: 'This is the code to verify your account',
 });
 
 job
